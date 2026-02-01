7bf972c028c23e4beb6e3f617cf0c4639c67f4bd
Mirzah-Muzammil <mirzaahmuzammil@gmail.com>
2026-02-01 14:49:20 +0530

    add

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
-const kBaseUrl = 'http://192.168.1.4:3000/api';
+const kBaseUrl = 'http://192.168.1.4:3000/api ';
 
 const kStudentLoginRoute = '$kBaseUrl/student/login';
 const kForgotPasswordRoute = '$kBaseUrl/student/forgot-password';
